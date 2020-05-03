c9b51a9c884f11789058df0ca26e08766f386a13
Thomas <thomas.foch@outlook.fr>
2020-05-03 09:48:31 +0200

    buy article form

--- UI/src/ui_package/bill_package/BuyArticle.java
+++ UI/src/ui_package/bill_package/BuyArticle.java
@@ -1,4 +1,22 @@
 package ui_package.bill_package;
 
+import javax.swing.*;
+
 public class BuyArticle {
+    private JPanel panelMain;
+    private JScrollPane scrollPaneTableDisplay;
+    private JTable tableDisplayArticle;
+    private JPanel panelSelectedArticle;
+    private JTable tableSelectedArticle;
+    private JButton buttonAddArticleSelected;
+    private JButton buttonRemoveArticleSelected;
+    private JComboBox comboBoxFamily;
+    private JButton buttonSortByFamily;
+    private JTextField textFieldGetArticleByReference;
+    private JButton buttonGetArticleByReference;
+    private JLabel labelTotal;
+    private JComboBox comboBoxPayBill;
+    private JButton buttonConfirm;
+    private JButton buttonCancel;
+    private final String shopName = "shop1";
 }
